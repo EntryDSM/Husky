e9b19189dadb56881d2a3f6d69aae0be0c671d68
Jaehoon Ki <by09115@outlook.kr>
2020-05-03 02:00:22 +0900

    [ADD] inheritance-applied CRUD repositories

---
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/applications/repositories/CalculatedScoreRepository.java
@@ -0,0 +1,9 @@
+package kr.hs.entrydsm.husky.entities.applications.repositories;
+
+import kr.hs.entrydsm.husky.entities.applications.CalculatedScore;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CalculatedScoreRepository extends ApplicationBaseRepository<CalculatedScore> {
+}
---
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/applications/repositories/UnGraduatedApplicationRepository.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.husky.entities.applications.repositories;
+
+import kr.hs.entrydsm.husky.entities.applications.UnGraduatedApplication;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnGraduatedApplicationRepository extends ApplicationBaseRepository<UnGraduatedApplication> {
+}
