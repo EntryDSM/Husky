4bf0f664084d93abc6a16805f754232ee3c5aa71
LeagueLugas <leaguelugas@gmail.com>
2020-06-09 09:17:43 +0900

    [FIX] add @Valid in request method argument

--- auth/src/main/java/kr/hs/entrydsm/husky/controllers/UserController.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/controllers/UserController.java
@@ -1,13 +1,14 @@
 package kr.hs.entrydsm.husky.controllers;
 
+import kr.hs.entrydsm.husky.domains.request.AccountRequest;
 import kr.hs.entrydsm.husky.domains.request.AuthCodeRequest;
 import kr.hs.entrydsm.husky.domains.request.PasswordRequest;
-import kr.hs.entrydsm.husky.domains.request.SignUpRequest;
 import kr.hs.entrydsm.husky.service.user.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
 @RestController
 @RequestMapping("/user")
@@ -17,24 +18,24 @@ public class UserController {
     private final UserServiceImpl signUpService;
 
     @PostMapping
-    public void signIn(@RequestBody @Valid SignUpRequest signUpRequest) {
-        signUpService.signUp(signUpRequest);
+    public void signIn(@RequestBody @Valid AccountRequest accountRequest) {
+        signUpService.signUp(accountRequest);
     }
 
     @PostMapping("/email/verify")
-    public void sendEmail(@RequestParam @Valid String email) {
+    public void sendEmail(@RequestParam("email") @Email String email) {
         signUpService.sendEmail(email);
     }
 
     @PutMapping("/email/verify")
-    public void authEmail(AuthCodeRequest authCodeRequest) {
+    public void authEmail(@RequestBody @Valid AuthCodeRequest authCodeRequest) {
         signUpService.authEmail(authCodeRequest);
     }
 
     @PutMapping("/{userId}/password")
     public void changePassword(@RequestHeader("Authorization") String token,
-                               @PathVariable Integer userId,
-                               @RequestParam PasswordRequest passwordRequest) {
+                               @PathVariable String userId,
+                               @RequestParam @Valid PasswordRequest passwordRequest) {
         signUpService.changePassword(token, userId, passwordRequest);
     }
 
