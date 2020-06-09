39b5eb0ea225b78e4d09af0ed8d8c839ca918c5b
LeagueLugas <leaguelugas@gmail.com>
2020-06-09 19:21:54 +0900

    [FIX] change request body name

--- auth/src/main/java/kr/hs/entrydsm/husky/domains/request/VerifyCodeRequest.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/domains/request/VerifyCodeRequest.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
-public class AuthCodeRequest {
+public class VerifyCodeRequest {
 
     @Email
     private String email;
