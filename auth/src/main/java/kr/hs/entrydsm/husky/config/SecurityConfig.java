6959ed542ebb3601219217e40bb84da1ed8dbdc4
LeagueLugas <leaguelugas@gmail.com>
2020-06-12 08:38:32 +0900

    [ADD] jwt config(filter) classes

--- auth/src/main/java/kr/hs/entrydsm/husky/config/SecurityConfig.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package kr.hs.entrydsm.husky.config;
 
+import kr.hs.entrydsm.husky.service.token.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,14 +13,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .formLogin().disable()
-                .headers().frameOptions().disable();
+                .authorizeRequests()
+                    .antMatchers("/auth").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
     }
 
     @Bean
