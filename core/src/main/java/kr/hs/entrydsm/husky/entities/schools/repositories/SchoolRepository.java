37d80d6ab59a275bcde745e0c124d1e4ef30c7d3
huewilliams <4879sinwoo@naver.com>
2020-06-18 20:00:48 +0900

    [ADD] school search method

--- core/src/main/java/kr/hs/entrydsm/husky/entities/schools/repositories/SchoolRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/schools/repositories/SchoolRepository.java
@@ -1,8 +1,11 @@
 package kr.hs.entrydsm.husky.entities.schools.repositories;
 
 import kr.hs.entrydsm.husky.entities.schools.School;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SchoolRepository extends JpaRepository<School, String> {
 
+    Page<School> findBySchoolFullNameContainsAndSchoolNameContains(String region, String name, Pageable pageable);
 }
