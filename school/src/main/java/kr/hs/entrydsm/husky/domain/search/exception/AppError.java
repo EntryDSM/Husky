931672bad5efe072c5fa3971193c9429c09ab478
huewilliams <4879sinwoo@naver.com>
2020-06-22 11:16:14 +0900

    [ADD] App Error

---
+++ school/src/main/java/kr/hs/entrydsm/husky/domain/search/exception/AppError.java
@@ -0,0 +1,12 @@
+package kr.hs.entrydsm.husky.domain.search.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public class AppError {
+    HttpStatus status;
+    String message;
+}
