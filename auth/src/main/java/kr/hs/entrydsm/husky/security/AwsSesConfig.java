e88ed08c79752a1cddcdfb6ebd7279e55840d43c
LeagueLugas <leaguelugas@gmail.com>
2020-06-26 14:53:19 +0900

    [ADD] aws ses config

---
+++ auth/src/main/java/kr/hs/entrydsm/husky/security/AwsSesConfig.java
@@ -0,0 +1,30 @@
+package kr.hs.entrydsm.husky.security;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsync;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${auth.email.AWS_ACCESS_KEY_ID}")
+    private String AWS_ACCESS_KEY_ID;
+
+    @Value("${auth.email.AWS_SECRET_KEY}")
+    private String AWS_SECRET_KEY;
+
+    @Bean
+    public AmazonSimpleEmailServiceAsync amazonSimpleEmailService() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
+
+        return AmazonSimpleEmailServiceAsyncClient.asyncBuilder()
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .withRegion(Regions.US_WEST_2)
+                .build();
+    }
+}
