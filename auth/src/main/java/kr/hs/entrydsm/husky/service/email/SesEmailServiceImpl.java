f21dadb63931e0df015180806efdd7de66045227
LeagueLugas <leaguelugas@gmail.com>
2020-07-02 09:09:27 +0900

    [FIX] email send code
    delete method that generate html code with code

--- auth/src/main/java/kr/hs/entrydsm/husky/service/email/SesEmailServiceImpl.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/service/email/SesEmailServiceImpl.java
@@ -16,40 +16,18 @@ import java.io.InputStreamReader;
 @RequiredArgsConstructor
 public class SesEmailServiceImpl implements EmailService {
 
-    @Value("${auth.email.send-email}")
-    private String senderEmail;
-
     private final AmazonSimpleEmailServiceAsync amazonSimpleEmailServiceAsync;
 
     @Override
     public void sendEmail(String receiveEmail, String code) {
-        String emailSubject = "2021학년도 신입생 입학원서 지원 시스템 - Entry 인증 메일";
-        String emailContent = createContent(code);
 
-
-        SendEmailRequest request = new SendEmailRequest()
+        SendTemplatedEmailRequest request = new SendTemplatedEmailRequest()
                 .withDestination(new Destination().withToAddresses(receiveEmail))
-                .withMessage(
-                        new Message()
-                        .withSubject(new Content().withCharset("UTF-8").withData(emailSubject))
-                        .withBody(new Body().withHtml(new Content().withCharset("UTF-8").withData(emailContent)))
-                )
-                .withSource(senderEmail);
+                .withTemplate("EntryEmailConfirmTemplate")
+                .withSource("=?utf-8?B?7J6F7ZWZ7KCE7ZiV7Iuc7Iqk7YWc?= <noreply@entrydsm.hs.kr>")
+                .withTemplateData("{\"code\": \""+code+"\"}");
 
-
-        amazonSimpleEmailServiceAsync.sendEmailAsync(request);
-
-    }
-
-    private String createContent(String code) {
-        StringBuilder result = new StringBuilder();
-        ClassPathResource resource = new ClassPathResource("verifyEmailForm.html");
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
-            bufferedReader.lines().forEach(result::append);
-        } catch (IOException e) {
-            throw new VerifyEmailGenerateException();
-        }
-        return result.toString().replace("{code}", code);
+        amazonSimpleEmailServiceAsync.sendTemplatedEmailAsync(request);
     }
 
 }
