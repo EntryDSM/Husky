329c50ccebd4e8d7beab9d5a58f6831f8dcf5ad7
LeagueLugas <leaguelugas@gmail.com>
2020-07-03 08:31:04 +0900

    [FIX] email template

--- auth/src/main/java/kr/hs/entrydsm/husky/service/email/SesEmailServiceImpl.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/service/email/SesEmailServiceImpl.java
@@ -18,7 +18,7 @@ public class SesEmailServiceImpl implements EmailService {
                 .withDestination(new Destination().withToAddresses(receiveEmail))
                 .withTemplate("EntryEmailConfirmTemplate")
                 .withSource("=?utf-8?B?7J6F7ZWZ7KCE7ZiV7Iuc7Iqk7YWc?= <noreply@entrydsm.hs.kr>")
-                .withTemplateData("{\"code\": \""+code+"\"}");
+                .withTemplateData("{\"code\": \""+code+"\", \"email\": \""+receiveEmail+"\"}");
 
         amazonSimpleEmailServiceAsync.sendTemplatedEmailAsync(request);
     }
