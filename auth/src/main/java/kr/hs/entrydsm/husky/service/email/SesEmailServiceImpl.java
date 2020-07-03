cc5badcf7a48cb487a548f3158cf6ac4526c32b7
LeagueLugas <leaguelugas@gmail.com>
2020-07-03 17:14:24 +0900

    AUTH - [FIX] division email sender name

--- auth/src/main/java/kr/hs/entrydsm/husky/service/email/SesEmailServiceImpl.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/service/email/SesEmailServiceImpl.java
@@ -11,16 +11,17 @@ public class SesEmailServiceImpl implements EmailService {
 
     private final AmazonSimpleEmailServiceAsync amazonSimpleEmailServiceAsync;
 
+    private static final String UTF_8_ENCODED_SOURCE_NAME = "=?utf-8?B?7J6F7ZWZ7KCE7ZiV7Iuc7Iqk7YWc?=";
+
     @Override
     public void sendEmail(String receiveEmail, String code) {
 
         SendTemplatedEmailRequest request = new SendTemplatedEmailRequest()
                 .withDestination(new Destination().withToAddresses(receiveEmail))
                 .withTemplate("EntryEmailConfirmTemplate")
-                .withSource("=?utf-8?B?7J6F7ZWZ7KCE7ZiV7Iuc7Iqk7YWc?= <noreply@entrydsm.hs.kr>")
+                .withSource(UTF_8_ENCODED_SOURCE_NAME + " <noreply@entrydsm.hs.kr>")
                 .withTemplateData("{\"code\": \""+code+"\", \"email\": \""+receiveEmail+"\"}");
 
         amazonSimpleEmailServiceAsync.sendTemplatedEmailAsync(request);
     }
-
 }
