ba9ae66f2eaab01f5702bb3a7de8e7d5f21f8ce8
LeagueLugas <leaguelugas@gmail.com>
2020-07-03 17:17:19 +0900

    AUTH - [FIX] change ses config class to package config

--- auth/src/main/java/kr/hs/entrydsm/husky/config/AwsSesConfig.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/config/AwsSesConfig.java
@@ -1,4 +1,4 @@
-package kr.hs.entrydsm.husky.security;
+package kr.hs.entrydsm.husky.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
