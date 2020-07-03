2ae5f85dee6f34e98e55f2900a4a31584c68964b
LeagueLugas <leaguelugas@gmail.com>
2020-07-03 17:11:03 +0900

    AUTH - [FIX] change excpetion response code

--- auth/src/main/java/kr/hs/entrydsm/husky/exceptions/VerifyEmailGenerateException.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/exceptions/VerifyEmailGenerateException.java
@@ -3,6 +3,6 @@ package kr.hs.entrydsm.husky.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "email create error")
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason = "email create error")
 public class VerifyEmailGenerateException extends RuntimeException {
 }
