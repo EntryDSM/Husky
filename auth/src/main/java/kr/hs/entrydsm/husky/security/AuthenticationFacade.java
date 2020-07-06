df5c1f8c6f204dd687af3e5d86c55950702c3e77
LeagueLugas <leaguelugas@gmail.com>
2020-07-06 09:33:20 +0900

    AUTH - [ADD] authentication facade

---
+++ auth/src/main/java/kr/hs/entrydsm/husky/security/AuthenticationFacade.java
@@ -0,0 +1,18 @@
+package kr.hs.entrydsm.husky.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade {
+
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    public String getUserEmail() {
+        return this.getAuthentication().getName();
+    }
+
+}
