9050e55eb996975a05c2e4664b1bd2e15dd4751e
LeagueLugas <leaguelugas@gmail.com>
2020-07-07 12:58:15 +0900

    AUTH - [FIX] add exception code

--- core/src/main/java/kr/hs/entrydsm/husky/error/exception/ErrorCode.java
+++ core/src/main/java/kr/hs/entrydsm/husky/error/exception/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     EXPIRED_AUTH_CODE(400, "VE1012", "Expired Auth Code"),
     INVALID_TOKEN(401, "", "Invalid Token"),
     EXPIRED_TOKEN(401, "", "Expired Token"),
+    UNAUTHORIZED(401, "", "Authentication is required and has failed or has not yet been provided."),
     USER_DUPLICATION(409, "VE1011", "User is Already Exists"),
     PASSWORD_DUPLICATION(409, "", "Password is Already Exists");
 
