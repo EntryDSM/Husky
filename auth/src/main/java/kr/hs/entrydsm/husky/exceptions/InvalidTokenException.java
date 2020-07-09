837084abd2d775b21d80e018f923c194a76caa26
LeagueLugas <leaguelugas@gmail.com>
2020-07-09 16:48:41 +0900

    AUTH - [FIX] exception : invalid token exception

--- auth/src/main/java/kr/hs/entrydsm/husky/exceptions/InvalidTokenException.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/exceptions/InvalidTokenException.java
@@ -3,7 +3,7 @@ package kr.hs.entrydsm.husky.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid Token")
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Invalid Token")
 public class InvalidTokenException extends RuntimeException {
     public InvalidTokenException() {
         super();
