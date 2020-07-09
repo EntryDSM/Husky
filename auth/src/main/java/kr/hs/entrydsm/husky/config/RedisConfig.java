8d3f0f28f0338db870ebf38cbc12545e24c43bf9
LeagueLugas <leaguelugas@gmail.com>
2020-07-09 16:49:37 +0900

    AUTH - [ADD] redis config

---
+++ auth/src/main/java/kr/hs/entrydsm/husky/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package kr.hs.entrydsm.husky.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+public class RedisConfig {
+}
