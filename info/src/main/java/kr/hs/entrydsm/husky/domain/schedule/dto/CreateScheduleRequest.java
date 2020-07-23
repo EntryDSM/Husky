287aacca1870f8709baa4903648e0ca3dd71e0fe
huewilliams <4879sinwoo@naver.com>
2020-07-23 10:46:21 +0900

    INFO - [ADD] create schedule request dto

---
+++ info/src/main/java/kr/hs/entrydsm/husky/domain/schedule/dto/CreateScheduleRequest.java
@@ -0,0 +1,25 @@
+package kr.hs.entrydsm.husky.domain.schedule.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateScheduleRequest {
+
+    private String id;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+}
