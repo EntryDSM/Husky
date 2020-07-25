653b6194ed524e3d878acc1c1c9d18dc4dc0df0f
huewilliams <4879sinwoo@naver.com>
2020-07-25 23:17:21 +0900

    INFO - [FIX] specify @Repository

--- info/src/main/java/kr/hs/entrydsm/husky/domain/schedule/dao/ScheduleRepository.java
+++ info/src/main/java/kr/hs/entrydsm/husky/domain/schedule/dao/ScheduleRepository.java
@@ -2,9 +2,11 @@ package kr.hs.entrydsm.husky.domain.schedule.dao;
 
 import kr.hs.entrydsm.husky.domain.schedule.domain.Schedule;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ScheduleRepository extends CrudRepository<Schedule, String> {
     List<Schedule> findAllByOrderByEndDate();
 }
