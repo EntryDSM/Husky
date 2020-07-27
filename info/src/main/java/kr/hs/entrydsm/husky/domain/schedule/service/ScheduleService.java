6e5b00c0dd797e3389a55cf0c65c6286a7b04114
huewilliams <4879sinwoo@naver.com>
2020-07-27 09:39:50 +0900

    INFO - [FIX] createSchedule service (check admin authorization)

--- info/src/main/java/kr/hs/entrydsm/husky/domain/schedule/service/ScheduleService.java
+++ info/src/main/java/kr/hs/entrydsm/husky/domain/schedule/service/ScheduleService.java
@@ -3,8 +3,12 @@ package kr.hs.entrydsm.husky.domain.schedule.service;
 import kr.hs.entrydsm.husky.domain.schedule.dao.ScheduleRepository;
 import kr.hs.entrydsm.husky.domain.schedule.domain.Schedule;
 import kr.hs.entrydsm.husky.domain.schedule.dto.CreateScheduleRequest;
+import kr.hs.entrydsm.husky.domain.schedule.exception.NotAdminException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 @RequiredArgsConstructor
 @Service
@@ -12,7 +16,18 @@ public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
 
-    public String createSchedule(CreateScheduleRequest request) {
+    @Value("${admin.secret.key}")
+    private String adminSecretKey;
+
+    public String createSchedule(CreateScheduleRequest request, String secret) {
+        byte[] targetBytes = secret.getBytes();
+        Base64.Encoder encoder = Base64.getEncoder();
+        byte[] encodeBytes = encoder.encode(targetBytes);
+
+        if(!adminSecretKey.equals(new String(encodeBytes))) {
+            throw new NotAdminException();
+        }
+
         Schedule schedule = scheduleRepository.save(
                 Schedule.builder()
                         .id(request.getId())
