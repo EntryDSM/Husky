5d25288fd583a1bbc6f3cd64c695b9dc6b124600
huewilliams <4879sinwoo@naver.com>
2020-07-31 13:53:04 +0900

    CORE - [FIX] User ID Type

--- core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/UserRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
