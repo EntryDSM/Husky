6b2aef0f08d9515e2dde5ac35065b0aafb9827bf
huewilliams <4879sinwoo@naver.com>
2020-08-02 20:46:15 +0900

    INFO - [FIX] apply @ActiveProfiles("test")

--- info/src/test/java/kr/hs/entrydsm/husky/UserTypeApiTest.java
+++ info/src/test/java/kr/hs/entrydsm/husky/UserTypeApiTest.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -36,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = InfoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class UserTypeApiTest {
 
     @LocalServerPort
