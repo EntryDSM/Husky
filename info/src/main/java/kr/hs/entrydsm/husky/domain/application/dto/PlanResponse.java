a5cedcd073f768da689dc32d647313baf7e54d80
huewilliams <4879sinwoo@naver.com>
2020-08-06 16:48:14 +0900

    INFO - [ADD] PlanResponse dto

---
+++ info/src/main/java/kr/hs/entrydsm/husky/domain/application/dto/PlanResponse.java
@@ -0,0 +1,14 @@
+package kr.hs.entrydsm.husky.domain.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlanResponse {
+    private String plan;
+}
