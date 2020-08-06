56d346ea2e9347d103a52794b3c429440fcbb5f7
huewilliams <4879sinwoo@naver.com>
2020-08-06 16:48:42 +0900

    INFO - [ADD] addPlan & getPlan service

--- info/src/main/java/kr/hs/entrydsm/husky/domain/application/service/ApplicationService.java
+++ info/src/main/java/kr/hs/entrydsm/husky/domain/application/service/ApplicationService.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.husky.domain.application.service;
 
 import kr.hs.entrydsm.husky.domain.application.dto.IntroResponse;
+import kr.hs.entrydsm.husky.domain.application.dto.PlanResponse;
 import kr.hs.entrydsm.husky.domain.user.exception.UserNotFoundException;
 import kr.hs.entrydsm.husky.entities.users.User;
 import kr.hs.entrydsm.husky.entities.users.repositories.UserRepository;
@@ -29,6 +30,23 @@ public class ApplicationService {
                 .orElseThrow(UserNotFoundException::new);
 
         return new IntroResponse(user.getSelfIntroduction());
+    }
+
+    public void addPlan(String plan) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        user.setStudyPlan(plan);
+        userRepository.save(user);
+    }
+
+    public PlanResponse getPlan() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        return new PlanResponse(user.getStudyPlan());
     }
 
 }
