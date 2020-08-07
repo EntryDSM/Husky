a2e3ac157a9c2a04e8b6239bf95fe1e6af21ed2b
huewilliams <4879sinwoo@naver.com>
2020-08-07 19:07:05 +0900

    INFO - [ADD] PATCH/applications/me/score/ged API

--- info/src/main/java/kr/hs/entrydsm/husky/domain/application/controller/ApplicationController.java
+++ info/src/main/java/kr/hs/entrydsm/husky/domain/application/controller/ApplicationController.java
@@ -3,13 +3,12 @@ package kr.hs.entrydsm.husky.domain.application.controller;
 import kr.hs.entrydsm.husky.domain.application.dto.IntroResponse;
 import kr.hs.entrydsm.husky.domain.application.dto.PlanResponse;
 import kr.hs.entrydsm.husky.domain.application.service.ApplicationService;
+import kr.hs.entrydsm.husky.error.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RequestMapping("/applications/me")
@@ -20,8 +19,10 @@ public class ApplicationController {
 
     @PatchMapping("/intro")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void addIntro(@Valid @NotBlank @NotEmpty @RequestBody String introduction) {
-        applicationService.addIntro(introduction);
+    public void addIntro(@RequestBody HashMap<String, String> request) {
+        checkParameter(request, "introduce");
+
+        applicationService.addIntro(request.get("introduce"));
     }
 
     @GetMapping("/intro")
@@ -31,13 +32,33 @@ public class ApplicationController {
 
     @PatchMapping("/plan")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void addPlan(@Valid @NotBlank @NotEmpty @RequestBody String plan) {
-        applicationService.addPlan(plan);
+    public void addPlan(@RequestBody HashMap<String, String> request) {
+        checkParameter(request, "plan");
+
+        applicationService.addPlan(request.get("plan"));
     }
 
     @GetMapping("/plan")
     public PlanResponse getPlan() {
         return applicationService.getPlan();
+    }
+
+    @PatchMapping("/score/ged")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void addGedScore(@RequestBody HashMap<String, String> request) {
+        checkParameter(request, "score");
+        int averageScore;
+        try {
+            averageScore = Integer.parseInt(request.get("score"));
+        } catch (Exception e) {
+            throw new BadRequestException();
+        }
+
+        applicationService.addGedScore(averageScore);
+    }
+
+    private void checkParameter(HashMap<String, String> request, String key) {
+        if(request.get(key) == null) throw new BadRequestException();
     }
 
 }
