5fec891c4b0fca230228a03ef8962f97b6d9ad36
huewilliams <4879sinwoo@naver.com>
2020-08-07 19:08:20 +0900

    INFO - [TEST] add_ged_score_api

--- info/src/test/java/kr/hs/entrydsm/husky/ApplicationApiTest.java
+++ info/src/test/java/kr/hs/entrydsm/husky/ApplicationApiTest.java
@@ -1,5 +1,14 @@
 package kr.hs.entrydsm.husky;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import kr.hs.entrydsm.husky.domain.user.dto.SelectTypeRequest;
+import kr.hs.entrydsm.husky.domain.user.exception.UserNotFoundException;
+import kr.hs.entrydsm.husky.entities.applications.GEDApplication;
+import kr.hs.entrydsm.husky.entities.applications.repositories.GEDApplicationRepository;
 import kr.hs.entrydsm.husky.entities.users.User;
 import kr.hs.entrydsm.husky.entities.users.repositories.UserRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -18,7 +27,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -35,6 +46,9 @@ class ApplicationApiTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private GEDApplicationRepository gedApplicationRepository;
 
     @Autowired
     private WebApplicationContext context;
@@ -80,9 +94,12 @@ class ApplicationApiTest {
                 .andExpect(status().isBadRequest());
 
         //then
+        HashMap<String, String> request = new HashMap<>();
+        request.put("introduce", "test");
+
         mvc.perform(patch(url + "/applications/me/intro")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("안녕하세요"))
+                .content(convertToJson(request)))
                 .andExpect(status().isNoContent());
 
         mvc.perform(get(url + "/applications/me/intro"))
@@ -100,13 +117,64 @@ class ApplicationApiTest {
                 .andExpect(status().isBadRequest());
 
         //then
+        HashMap<String, String> request = new HashMap<>();
+        request.put("plan", "test");
+
         mvc.perform(patch(url + "/applications/me/plan")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("안녕하세요"))
+                .content(convertToJson(request)))
                 .andExpect(status().isNoContent());
 
         mvc.perform(get(url + "/applications/me/plan"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "test6", password = "1234")
+    public void add_ged_score_api() throws Exception {
+        //given
+        String url = "http://localhost:" + port;
+
+        //when
+        select_user_type(url, "GED");
+
+        HashMap<String, String> request = new HashMap<>();
+        request.put("score", "3100");
+
+        mvc.perform(patch(url + "/applications/me/score/ged")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertToJson(request)))
+                .andExpect(status().isNoContent());
+
+        GEDApplication application = gedApplicationRepository.findByEmail("test6")
+                .orElseThrow(UserNotFoundException::new);
+        System.out.println(application.getGedAverageScore());
+    }
+
+    private String convertToJson(Object object) throws JsonProcessingException {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .writeValueAsString(object);
+    }
+
+    private void select_user_type(String url, String gradeType) throws Exception {
+        mvc.perform(patch(url + "/users/me/type")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper()
+                        .registerModule(new JavaTimeModule())
+                        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                        .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                        .writeValueAsString(SelectTypeRequest.builder()
+                                .gradeType(gradeType)
+                                .applyType("COMMON")
+                                .additionalType("NOT_APPLICABLE")
+                                .isDaejeon(true)
+                                .gedPassDate(LocalDate.parse("2020-02-20"))
+                                .graduatedDate(LocalDate.parse("2020-02-20"))
+                                .build()))
+        );
     }
 
 }
