8b4bb0e1699678dc9c0f0348fac40e1c4793d788
LumpKim <me@jaehoon.kim>
2020-08-10 18:31:00 +0900

    [CLEAN] useless inherit relationship on CalculatedScore

--- core/src/main/java/kr/hs/entrydsm/husky/entities/applications/CalculatedScore.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/applications/CalculatedScore.java
@@ -1,26 +1,57 @@
 package kr.hs.entrydsm.husky.entities.applications;
 
+import kr.hs.entrydsm.husky.entities.users.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
-@Builder
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class CalculatedScore extends Application {
+@Entity
+public class CalculatedScore {
 
+    @Id
+    @Column(name = "user_receipt_code")
+    private Integer receiptCode;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime modifiedAt;
+
+    @Digits(integer = 3, fraction = 3)
     private BigDecimal volunteerScore;
 
     private Integer attendanceScore;
 
+    @Digits(integer = 3, fraction = 3)
     private BigDecimal conversionScore;
 
+    @Digits(integer = 3, fraction = 3)
     private BigDecimal finalScore;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private User user;
+
+    @Builder
+    public CalculatedScore(User user, BigDecimal volunteerScore, Integer attendanceScore, BigDecimal conversionScore, BigDecimal finalScore) {
+        this.user = user;
+        this.receiptCode = user.getReceiptCode();
+        this.volunteerScore = volunteerScore;
+        this.attendanceScore = attendanceScore;
+        this.conversionScore = conversionScore;
+        this.finalScore = finalScore;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
 
 }
