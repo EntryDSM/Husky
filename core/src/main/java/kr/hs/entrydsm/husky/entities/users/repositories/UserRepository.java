15d8371c38df540cdeb20fa3f72b4b1e5c05648a
LumpKim <me@jaehoon.kim>
2020-08-10 18:23:30 +0900

    [ADD] UserRepository::findByReceiptCode

--- core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/UserRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByReceiptCode(int receiptCode);
+
 }
