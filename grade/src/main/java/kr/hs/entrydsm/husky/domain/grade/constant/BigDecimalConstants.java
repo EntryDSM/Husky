1bbf9b788f5f082bfa680f4299fcaa8089838ae0
LumpKim <me@jaehoon.kim>
2020-08-10 18:28:03 +0900

    [ADD] constants for GradeCalcService

---
+++ grade/src/main/java/kr/hs/entrydsm/husky/domain/grade/constant/BigDecimalConstants.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.husky.domain.grade.constant;
+
+import java.math.BigDecimal;
+
+public class BigDecimalConstants {
+
+    public static final BigDecimal TWO = BigDecimal.valueOf(2);
+
+    public static final BigDecimal FOUR = BigDecimal.valueOf(4);
+
+    public static final BigDecimal SIX = BigDecimal.valueOf(6);
+
+}
