b682b18843f0ab7997e1eb912cbb9096099274ff
LumpKim <me@jaehoon.kim>
2020-08-10 18:23:10 +0900

    [FIX] extended interface to JpaRepository from CrudRepository

--- core/src/main/java/kr/hs/entrydsm/husky/entities/applications/repositories/CalculatedScoreRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/applications/repositories/CalculatedScoreRepository.java
@@ -1,9 +1,10 @@
 package kr.hs.entrydsm.husky.entities.applications.repositories;
 
 import kr.hs.entrydsm.husky.entities.applications.CalculatedScore;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CalculatedScoreRepository extends ApplicationBaseRepository<CalculatedScore> {
+public interface CalculatedScoreRepository extends JpaRepository<CalculatedScore, Integer> {
 }
