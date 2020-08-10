0e48d584bd754ab90703349404f7864467ee79d5
huewilliams <4879sinwoo@naver.com>
2020-08-10 09:25:01 +0900

    INFO - [ADD] ScoreResponse dto

---
+++ info/src/main/java/kr/hs/entrydsm/husky/domain/application/dto/ScoreResponse.java
@@ -0,0 +1,59 @@
+package kr.hs.entrydsm.husky.domain.application.dto;
+
+import kr.hs.entrydsm.husky.entities.applications.GEDApplication;
+import kr.hs.entrydsm.husky.entities.applications.GeneralApplication;
+import kr.hs.entrydsm.husky.entities.users.enums.GradeType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScoreResponse {
+
+    private GradeType gradeType;
+
+    private int gedAverageScore;
+
+    private int volunteerTime;
+    private int fullCutCount;
+    private int periodCutCount;
+    private int lateCount;
+    private int earlyLeaveCount;
+    private String korean;
+    private String social;
+    private String history;
+    private String math;
+    private String science;
+    private String techAndHome;
+    private String english;
+
+    public static ScoreResponse gedResponse(GEDApplication gedApplication, GradeType gradeType) {
+        return ScoreResponse.builder()
+                .gradeType(gradeType)
+                .gedAverageScore(gedApplication.getGedAverageScore())
+                .build();
+    }
+
+    public static ScoreResponse response(GeneralApplication application, GradeType gradeType) {
+        return ScoreResponse.builder()
+                .gradeType(gradeType)
+                .volunteerTime(application.getVolunteerTime())
+                .fullCutCount(application.getFullCutCount())
+                .periodCutCount(application.getPeriodCutCount())
+                .lateCount(application.getLateCount())
+                .earlyLeaveCount(application.getEarlyLeaveCount())
+                .korean(application.getKorean())
+                .social(application.getSocial())
+                .history(application.getHistory())
+                .math(application.getMath())
+                .science(application.getScience())
+                .techAndHome(application.getTechAndHome())
+                .english(application.getEnglish())
+                .build();
+    }
+
+}
