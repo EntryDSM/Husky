c4bec21e62408e1717e03af2463e567b161686bd
LumpKim <me@jaehoon.kim>
2020-08-12 17:20:47 +0900

    [DELETE] `@ActiveProfiles` on GradeCalcTest

--- grade/src/test/java/kr/hs/entrydsm/husky/GradeCalcTest.java
+++ grade/src/test/java/kr/hs/entrydsm/husky/GradeCalcTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
@@ -31,7 +30,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = GradeApplication.class)
-@ActiveProfiles("test")
 public class GradeCalcTest {
 
     @Autowired
