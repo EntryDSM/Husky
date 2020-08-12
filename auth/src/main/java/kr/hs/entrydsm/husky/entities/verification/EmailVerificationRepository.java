db7b14a7e6e9abf7733105683c9556c0f177595b
LeagueLugas <leaguelugas@gmail.com>
2020-08-12 22:14:19 +0900

    AUTH - [FIX] repository : change extended class

--- auth/src/main/java/kr/hs/entrydsm/husky/entities/verification/EmailVerificationRepository.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/entities/verification/EmailVerificationRepository.java
@@ -1,8 +1,8 @@
 package kr.hs.entrydsm.husky.entities.verification;
 
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmailVerificationRepository extends KeyValueRepository<EmailVerification, String> {
+public interface EmailVerificationRepository extends CrudRepository<EmailVerification, String> {
 }
