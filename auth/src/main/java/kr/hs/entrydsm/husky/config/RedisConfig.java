7ddc5b729cfc2e7bed70b2bdc903a8ac324ff786
LeagueLugas <leaguelugas@gmail.com>
2020-08-12 22:13:48 +0900

    AUTH - [ADD] redis config : profile

--- auth/src/main/java/kr/hs/entrydsm/husky/config/RedisConfig.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/config/RedisConfig.java
@@ -2,11 +2,13 @@ package kr.hs.entrydsm.husky.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Profile("local")
 @Configuration
 public class RedisConfig {
 
