e89307df46d8f4cc1259f9680503bbca11c90ae8
LeagueLugas <leaguelugas@gmail.com>
2020-08-13 12:54:33 +0900

    AUTH - [FIX] change super repository

--- auth/src/main/java/kr/hs/entrydsm/husky/entities/refresh_token/RefreshTokenRepository.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/entities/refresh_token/RefreshTokenRepository.java
@@ -1,11 +1,11 @@
 package kr.hs.entrydsm.husky.entities.refresh_token;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
