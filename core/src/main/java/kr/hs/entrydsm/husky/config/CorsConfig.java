a35b66dd723bcff2fbaf26e28f6376ad4ab5e837
huewilliams <4879sinwoo@naver.com>
2020-08-14 21:49:10 +0900

    CORE - [ADD] Cors allow configuration

---
+++ core/src/main/java/kr/hs/entrydsm/husky/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package kr.hs.entrydsm.husky.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
