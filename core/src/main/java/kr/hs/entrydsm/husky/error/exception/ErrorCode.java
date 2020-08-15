c0cc58cd3d9bf7885855bf50aea2e04dcd7053af
huewilliams <4879sinwoo@naver.com>
2020-08-15 21:54:05 +0900

    CORE - [FIX] Error Code assign by new errorcode rule

--- core/src/main/java/kr/hs/entrydsm/husky/error/exception/ErrorCode.java
+++ core/src/main/java/kr/hs/entrydsm/husky/error/exception/ErrorCode.java
@@ -7,27 +7,28 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     //Global
-    USER_NOT_FOUND(404, "", "User Not Found."),
-    BAD_REQUEST(400, "", "Bad Request(Invalid Parameter)"),
+
+    BAD_REQUEST(400, "G400-0", "Bad Request(Invalid Parameter)"),
+    USER_NOT_FOUND(404, "G404-0", "User Not Found."),
 
     //Auth
-    INVALID_AUTH_EMAIL(400, "VE1010", "Invalid Auth Email"),
-    INVALID_AUTH_CODE(400, "VE1013", "Invalid Auth Code"),
-    EXPIRED_AUTH_CODE(400, "VE1012", "Expired Auth Code"),
-    INVALID_TOKEN(401, "", "Invalid Token"),
-    EXPIRED_TOKEN(401, "", "Expired Token"),
-    UNAUTHORIZED(401, "", "Authentication is required and has failed or has not yet been provided."),
-    USER_DUPLICATION(409, "VE1011", "User is Already Exists"),
-    PASSWORD_DUPLICATION(409, "", "Password is Already Exists"),
-    FAIL_GENERATE_VERIFY_EMAIL(422, "", "Email Generate Error"),
+    INVALID_AUTH_EMAIL(400, "A400-0", "Invalid Auth Email"),
+    INVALID_AUTH_CODE(400, "A400-1", "Invalid Auth Code"),
+    EXPIRED_AUTH_CODE(400, "A400-2", "Expired Auth Code"),
+    INVALID_TOKEN(401, "A401-0", "Invalid Token"),
+    EXPIRED_TOKEN(401, "A401-1", "Expired Token"),
+    UNAUTHORIZED(401, "A401-2", "Authentication is required and has failed or has not yet been provided."),
+    USER_DUPLICATION(409, "A409-0", "User is Already Exists"),
+    PASSWORD_DUPLICATION(409, "A409-1", "Password is Already Exists"),
+    FAIL_GENERATE_VERIFY_EMAIL(422, "A422-0", "Email Generate Error"),
 
     //Info
-    NOT_ADMIN_FORBIDDEN(403, "", "The server understood the request but refuses to authorize it."),
-    APPLICATION_NOT_FOUND(404, "", "Application Not Found"),
-    APPLICATION_TYPE_UNMATCHED(403, "", "Application Type is unmatched"),
+    NOT_ADMIN_FORBIDDEN(403, "I403-0", "The server understood the request but refuses to authorize it."),
+    APPLICATION_TYPE_UNMATCHED(403, "I403-1", "Application Type is unmatched"),
+    APPLICATION_NOT_FOUND(404, "I404-0", "Application Not Found"),
 
     //School
-    SCHOOL_NOT_FOUND(404, "", "School Not Found.");
+    SCHOOL_NOT_FOUND(404, "S404-0", "School Not Found.");
 
     private final int status;
     private final String code;
