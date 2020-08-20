1f67702adb5f6ac07eb44fcca0453af34cf4ae4d
LeagueLugas <leaguelugas@gmail.com>
2020-08-20 12:06:17 +0900

    CORE - [ADD] user repository : findByReceiptCode() method

--- core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/UserRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByReceiptCode(Integer receiptCode);
 }
