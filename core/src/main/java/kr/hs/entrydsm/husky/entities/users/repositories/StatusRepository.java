ca590ed93c1a402cf55abefc72fba077ded2d292
LumpKim <me@jaehoon.kim>
2020-08-22 00:55:00 +0900

    CORE - [DELETE] StatusRepository::findByEmail

--- core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/StatusRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/StatusRepository.java
@@ -1,13 +1,9 @@
 package kr.hs.entrydsm.husky.entities.users.repositories;
 
 import kr.hs.entrydsm.husky.entities.users.Status;
-import kr.hs.entrydsm.husky.entities.users.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface StatusRepository extends CrudRepository<Status, String> {
-    Optional<Status> findByEmail(String email);
 }
