f83eaa53edfce14a6a8b63788f877cca7d09e8f1
LeagueLugas <leaguelugas@gmail.com>
2020-08-22 21:47:08 +0900

    CORE - [FIX] Authentication facade : chane get receipt code logic

--- core/src/main/java/kr/hs/entrydsm/husky/security/AuthenticationFacade.java
+++ core/src/main/java/kr/hs/entrydsm/husky/security/AuthenticationFacade.java
@@ -1,19 +1,27 @@
 package kr.hs.entrydsm.husky.security;
 
 import kr.hs.entrydsm.husky.entities.users.User;
+import kr.hs.entrydsm.husky.entities.users.repositories.UserRepository;
+import kr.hs.entrydsm.husky.error.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AuthenticationFacade {
+
+    private final UserRepository userRepository;
 
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
     public Integer getReceiptCode() {
-        return ((User) this.getAuthentication().getPrincipal()).getReceiptCode();
+        User user = userRepository.findByEmail(this.getAuthentication().getName())
+                .orElseThrow(UserNotFoundException::new);
+        return user.getReceiptCode();
     }
 
 }
