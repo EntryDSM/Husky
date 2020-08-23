db2c69e6009f90da7671c453a39796376f87bb82
LeagueLugas <leaguelugas@gmail.com>
2020-08-23 21:22:04 +0900

    CORE - [FIX] get name method

--- core/src/main/java/kr/hs/entrydsm/husky/security/AuthenticationFacade.java
+++ core/src/main/java/kr/hs/entrydsm/husky/security/AuthenticationFacade.java
@@ -1,27 +1,18 @@
 package kr.hs.entrydsm.husky.security;
 
-import kr.hs.entrydsm.husky.entities.users.User;
-import kr.hs.entrydsm.husky.entities.users.repositories.UserRepository;
-import kr.hs.entrydsm.husky.error.exception.UserNotFoundException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class AuthenticationFacade {
-
-    private final UserRepository userRepository;
 
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
     public Integer getReceiptCode() {
-        User user = userRepository.findByEmail(this.getAuthentication().getName())
-                .orElseThrow(UserNotFoundException::new);
-        return user.getReceiptCode();
+        return Integer.parseInt(this.getAuthentication().getName());
     }
 
 }
