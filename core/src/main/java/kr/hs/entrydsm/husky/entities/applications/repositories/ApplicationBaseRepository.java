2262623b998745deebb59dd356f9d4b752cf7c48
LeagueLugas <leaguelugas@gmail.com>
2020-08-23 10:32:31 +0900

    CORE - [DELETE] application base repository : orm

--- core/src/main/java/kr/hs/entrydsm/husky/entities/applications/repositories/ApplicationBaseRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/applications/repositories/ApplicationBaseRepository.java
@@ -4,11 +4,6 @@ import kr.hs.entrydsm.husky.entities.applications.Application;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface ApplicationBaseRepository<T extends Application> extends CrudRepository<T, String> {
-
-    Optional<T> findByReceiptCode(Integer receiptCode);
-
+public interface ApplicationBaseRepository<T extends Application> extends CrudRepository<T, Integer> {
 }
