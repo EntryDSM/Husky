7c769edade4dd053796d952031b0b0721729b962
LeagueLugas <leaguelugas@gmail.com>
2020-08-23 10:31:39 +0900

    CORE - [FIX] status repository pk type

--- core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/StatusRepository.java
+++ core/src/main/java/kr/hs/entrydsm/husky/entities/users/repositories/StatusRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StatusRepository extends CrudRepository<Status, String> {
+public interface StatusRepository extends CrudRepository<Status, Integer> {
 }
