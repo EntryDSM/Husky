0a32195420424561875f1e9565fe3d9bd57078ea
LumpKim <me@jaehoon.kim>
2020-08-28 15:28:55 +0900

    AUTH - [ADD] javax.validation annotation

--- auth/src/main/java/kr/hs/entrydsm/husky/dto/request/AccountRequest.java
+++ auth/src/main/java/kr/hs/entrydsm/husky/dto/request/AccountRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 @Getter
@@ -13,8 +14,10 @@ import javax.validation.constraints.Pattern;
 public class AccountRequest {
 
     @Email
+    @NotEmpty
     private String email;
 
+    @NotEmpty
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
     private String password;
 
