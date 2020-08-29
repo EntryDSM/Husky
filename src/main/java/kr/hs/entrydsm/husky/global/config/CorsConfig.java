4d4a932ef56a98d28e932990010a15d822afcf3a
LumpKim <me@jaehoon.kim>
2020-08-29 21:45:28 +0900

    [ADD] CORS Configuration

---
+++ src/main/java/kr/hs/entrydsm/husky/global/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package kr.hs.entrydsm.husky.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+
+}
