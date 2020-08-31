4b004fef430f8f26d403297f161fc4ce0543871f
huewilliams <4879sinwoo@naver.com>
2020-08-31 11:46:28 +0900

    INFO - [FIX] embeddedRedisConfig

--- src/main/java/kr/hs/entrydsm/husky/infra/redis/EmbeddedRedisConfig.java
+++ src/main/java/kr/hs/entrydsm/husky/infra/redis/EmbeddedRedisConfig.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.husky.infra.redis;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,25 +8,69 @@ import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-@Configuration
 @Profile("local")
+@Configuration
 public class EmbeddedRedisConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     private RedisServer redisServer;
 
-    public EmbeddedRedisConfig(@Value("${spring.redis.port}") int redisPort) {
-        this.redisServer = new RedisServer(redisPort);
-    }
-
     @PostConstruct
-    public void runRedis() {
+    public void redisServer() throws IOException {
+        int port = isRedisRunning()? findAvailablePort() : redisPort;
+        redisServer = new RedisServer(port);
         redisServer.start();
     }
 
     @PreDestroy
     public void stopRedis() {
-        redisServer.stop();
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    public int findAvailablePort() throws IOException {
+
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+
+        } catch (Exception e) {
+        }
+
+        return !StringUtils.isEmpty(pidInfo.toString());
     }
 
 }
