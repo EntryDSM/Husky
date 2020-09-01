6c31dcb0176765ddedd9a89e691e1829d88e1794
LumpKim <me@jaehoon.kim>
2020-09-01 09:49:44 +0900

    [ADD] split 401 and 5xx on ExceptionHandlerFilter

--- src/main/java/kr/hs/entrydsm/husky/global/config/security/ExceptionHandlerFilter.java
+++ src/main/java/kr/hs/entrydsm/husky/global/config/security/ExceptionHandlerFilter.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.husky.global.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.hs.entrydsm.husky.domain.auth.exceptions.ExpiredTokenException;
 import kr.hs.entrydsm.husky.global.error.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,15 +17,22 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
             filterChain.doFilter(request, response);
-        } catch (ServletException | IOException e) {
-            ObjectMapper objectMapper = new ObjectMapper();
+        } catch (ExpiredTokenException e) {
             String jsonValue = objectMapper.writer()
                     .writeValueAsString(ErrorCode.EXPIRED_TOKEN);
             response.getWriter().write(jsonValue);
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            e.printStackTrace();
+        } catch (ServletException e) {
+            String jsonValue = objectMapper.writer()
+                    .writeValueAsString(ErrorCode.INTERNAL_SERVER_ERROR);
+            response.getWriter().write(jsonValue);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             e.printStackTrace();
         }
     }
