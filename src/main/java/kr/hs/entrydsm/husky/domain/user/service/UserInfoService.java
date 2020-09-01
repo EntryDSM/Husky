5b596799f7510d8066f6a380861e69511e437225
LumpKim <me@jaehoon.kim>
2020-09-01 10:10:30 +0900

    [FIX] return user info even if there is no application information

--- src/main/java/kr/hs/entrydsm/husky/domain/user/service/UserInfoService.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/user/service/UserInfoService.java
@@ -1,21 +1,15 @@
 package kr.hs.entrydsm.husky.domain.user.service;
 
 import kr.hs.entrydsm.husky.domain.application.domain.GeneralApplication;
-import kr.hs.entrydsm.husky.domain.application.exception.ApplicationNotFoundException;
+import kr.hs.entrydsm.husky.domain.application.domain.repositories.GeneralApplicationRepository;
+import kr.hs.entrydsm.husky.domain.school.domain.School;
+import kr.hs.entrydsm.husky.domain.school.domain.repositories.SchoolRepository;
+import kr.hs.entrydsm.husky.domain.user.domain.User;
+import kr.hs.entrydsm.husky.domain.user.domain.repositories.UserRepository;
 import kr.hs.entrydsm.husky.domain.user.dto.SetUserInfoRequest;
 import kr.hs.entrydsm.husky.domain.user.dto.UserInfoResponse;
 import kr.hs.entrydsm.husky.domain.user.exception.SchoolNotFoundException;
 import kr.hs.entrydsm.husky.domain.user.exception.UserNotFoundException;
-import kr.hs.entrydsm.husky.domain.application.domain.GraduatedApplication;
-import kr.hs.entrydsm.husky.domain.application.domain.UnGraduatedApplication;
-import kr.hs.entrydsm.husky.domain.application.domain.repositories.GEDApplicationRepository;
-import kr.hs.entrydsm.husky.domain.application.domain.repositories.GraduatedApplicationRepository;
-import kr.hs.entrydsm.husky.domain.application.domain.repositories.UnGraduatedApplicationRepository;
-import kr.hs.entrydsm.husky.domain.school.domain.School;
-import kr.hs.entrydsm.husky.domain.school.domain.repositories.SchoolRepository;
-import kr.hs.entrydsm.husky.domain.user.domain.User;
-import kr.hs.entrydsm.husky.domain.user.domain.enums.Sex;
-import kr.hs.entrydsm.husky.domain.user.domain.repositories.UserRepository;
 import kr.hs.entrydsm.husky.global.config.security.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,9 +21,7 @@ import javax.transaction.Transactional;
 public class UserInfoService {
 
     private final UserRepository userRepository;
-    private final GEDApplicationRepository gedRepository;
-    private final GraduatedApplicationRepository graduatedRepository;
-    private final UnGraduatedApplicationRepository unGraduatedRepository;
+    private final GeneralApplicationRepository generalApplicationRepository;
     private final SchoolRepository schoolRepository;
 
     private final AuthenticationFacade authenticationFacade;
@@ -40,39 +32,34 @@ public class UserInfoService {
         User user = userRepository.findById(receiptCode)
                 .orElseThrow(UserNotFoundException::new);
 
-        user.setInfo(request.getName(), Sex.valueOf(request.getSex()), request.getBirthDate(),
-                request.getApplicantTel(), request.getParentTel(), request.getParentName(), request.getAddress(),
-                request.getDetailAddress(), request.getPostCode(), request.getPhoto());
-        userRepository.save(user);
+        user.update(request);
+        user = userRepository.save(user);
 
-        switch (user.getGradeType()) {
-            case UNGRADUATED: {
-                UnGraduatedApplication unGraduated = unGraduatedRepository.findById(user.getReceiptCode())
-                        .orElseThrow(ApplicationNotFoundException::new);
-                School school = schoolRepository.findById(request.getSchoolCode())
-                        .orElseThrow(SchoolNotFoundException::new);
-
-                unGraduated.setStudentInfo(request.getStudentNumber(), school, request.getSchoolTel());
-                unGraduatedRepository.save(unGraduated);
-
-                return UserInfoResponse.response(user, unGraduated.getStudentNumber(),
-                        unGraduated.getSchool().getSchoolCode(), unGraduated.getSchoolTel());
-            }
-            case GRADUATED: {
-                GraduatedApplication graduated = graduatedRepository.findById(user.getReceiptCode())
-                        .orElseThrow(ApplicationNotFoundException::new);
-                School school = schoolRepository.findById(request.getSchoolCode())
-                        .orElseThrow(SchoolNotFoundException::new);
-
-                graduated.setStudentInfo(request.getStudentNumber(), school, request.getSchoolTel());
-                graduatedRepository.save(graduated);
-
-                return UserInfoResponse.response(user, graduated.getStudentNumber(),
-                        graduated.getSchool().getSchoolCode(), graduated.getSchoolTel());
-            }
+        if (isGradeTypeEmpty(user) || user.isGED()) {
+            return UserInfoResponse.builder()
+                    .user(user).build();
         }
 
-        return UserInfoResponse.response(user, null, null, null);
+        GeneralApplication application = user.getGeneralApplication();
+        if (!isSchoolCodeEmpty(request)) {
+            School school = schoolRepository.findById(request.getSchoolCode())
+                    .orElseThrow(SchoolNotFoundException::new);
+            application.update(school);
+        }
+
+        application.update(request);
+        generalApplicationRepository.save(application);
+
+        return UserInfoResponse.builder()
+                .user(user)
+                .studentNumber(application.getStudentNumber())
+                .schoolCode(application.getSchool().getSchoolCode())
+                .schoolTel(application.getSchoolTel())
+                .build();
+    }
+
+    public boolean isSchoolCodeEmpty(SetUserInfoRequest request) {
+        return request.getSchoolCode() == null;
     }
 
     public UserInfoResponse getUserInfo() {
@@ -80,15 +67,26 @@ public class UserInfoService {
         User user = userRepository.findById(receiptCode)
                 .orElseThrow(UserNotFoundException::new);
 
-        if (user.getGradeType() == null || user.isGED()) {
-            return UserInfoResponse.response(user, null, null, null);
+        if (isGradeTypeEmpty(user) || user.isGED() || isGeneralApplicationEmpty(user)) {
+            return UserInfoResponse.builder()
+                    .user(user).build();
         }
 
         GeneralApplication application = user.getGeneralApplication();
-        if(application == null) throw new ApplicationNotFoundException();
+        return UserInfoResponse.builder()
+                .user(user)
+                .studentNumber(application.getStudentNumber())
+                .schoolCode(application.getSchool().getSchoolCode())
+                .schoolTel(application.getSchoolTel())
+                .build();
+    }
 
-        return UserInfoResponse.response(user, application.getStudentNumber(),
-                application.getSchool().getSchoolCode(), application.getSchoolTel());
+    private boolean isGradeTypeEmpty(User user) {
+        return user.getGradeType() == null;
+    }
+
+    private boolean isGeneralApplicationEmpty(User user) {
+        return user.getGeneralApplication() == null;
     }
 
 }
