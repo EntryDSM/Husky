e34a716c2c2fa63fb9641eba920243b9f3b4900a
LumpKim <me@jaehoon.kim>
2020-09-01 14:13:18 +0900

    [RENAME] auth controllers

--- src/main/java/kr/hs/entrydsm/husky/domain/auth/controllers/AuthController.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/auth/controllers/AuthController.java
@@ -1,28 +1,45 @@
 package kr.hs.entrydsm.husky.domain.auth.controllers;
 
 import kr.hs.entrydsm.husky.domain.auth.dto.request.AccountRequest;
-import kr.hs.entrydsm.husky.domain.auth.dto.response.TokenResponse;
-import kr.hs.entrydsm.husky.domain.auth.service.auth.AuthServiceImpl;
+import kr.hs.entrydsm.husky.domain.auth.dto.request.VerifyCodeRequest;
+import kr.hs.entrydsm.husky.domain.auth.dto.request.ChangePasswordRequest;
+import kr.hs.entrydsm.husky.domain.auth.service.user.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthServiceImpl authService;
+    private final UserServiceImpl userService;
 
     @PostMapping
-    public TokenResponse signIn(@RequestBody @Valid AccountRequest accountRequest) {
-        return authService.signIn(accountRequest);
+    public void signUp(@RequestBody @Valid AccountRequest accountRequest) {
+        userService.signUp(accountRequest);
     }
 
-    @PutMapping
-    public TokenResponse refreshToken(@RequestHeader("X-Refresh-Token") String refreshToken) {
-        return authService.refreshToken(refreshToken);
+    @PostMapping("/email/verify")
+    public void sendEmail(@RequestParam("email") @Email String email) {
+        userService.sendSignUpEmail(email);
+    }
+
+    @PutMapping("/email/verify")
+    public void verifyEmail(@RequestBody @Valid VerifyCodeRequest verifyCodeRequest) {
+        userService.authEmail(verifyCodeRequest);
+    }
+
+    @PostMapping("/email/password/verify")
+    public void sendPasswordChangeEmail(@RequestParam("email") @Email String email) {
+        userService.sendPasswordChangeEmail(email);
+    }
+
+    @PutMapping("/password")
+    public void changePassword(@RequestBody @Valid ChangePasswordRequest changePasswordRequest) {
+        userService.changePassword(changePasswordRequest);
     }
 
 }
---
+++ src/main/java/kr/hs/entrydsm/husky/domain/auth/controllers/TokenController.java
@@ -0,0 +1,29 @@
+package kr.hs.entrydsm.husky.domain.auth.controllers;
+
+import kr.hs.entrydsm.husky.domain.auth.dto.request.AccountRequest;
+import kr.hs.entrydsm.husky.domain.auth.dto.request.SignInRequest;
+import kr.hs.entrydsm.husky.domain.auth.dto.response.TokenResponse;
+import kr.hs.entrydsm.husky.domain.auth.service.auth.AuthServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class TokenController {
+
+    private final AuthServiceImpl authService;
+
+    @PostMapping
+    public TokenResponse signIn(@RequestBody @Valid SignInRequest dto) {
+        return authService.signIn(dto);
+    }
+
+    @PutMapping
+    public TokenResponse refreshToken(@RequestHeader("X-Refresh-Token") String refreshToken) {
+        return authService.refreshToken(refreshToken);
+    }
+
+}
