9f660e149cc9fbf3fd431b3099558890f7e5198a
LumpKim <me@jaehoon.kim>
2020-09-01 14:10:55 +0900

    [EXCLUDE] catch ServletException on ExceptionHandlerFilter

--- src/main/java/kr/hs/entrydsm/husky/global/config/security/ExceptionHandlerFilter.java
+++ src/main/java/kr/hs/entrydsm/husky/global/config/security/ExceptionHandlerFilter.java
@@ -16,23 +16,16 @@ import java.io.IOException;
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
         } catch (ExpiredTokenException e) {
+            ObjectMapper objectMapper = new ObjectMapper();
             String jsonValue = objectMapper.writer()
                     .writeValueAsString(ErrorCode.EXPIRED_TOKEN);
             response.getWriter().write(jsonValue);
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            e.printStackTrace();
-        } catch (ServletException e) {
-            String jsonValue = objectMapper.writer()
-                    .writeValueAsString(ErrorCode.INTERNAL_SERVER_ERROR);
-            response.getWriter().write(jsonValue);
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             e.printStackTrace();
         }
     }
