8e581d82df01ce0cf6784e6c6e62017a91c4e567
LumpKim <me@jaehoon.kim>
2020-09-03 18:48:25 +0900

    [DELETE] useless repositories

--- src/main/java/kr/hs/entrydsm/husky/domain/application/domain/repositories/ApplicationBaseRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.hs.entrydsm.husky.domain.application.domain.repositories;
-
-import kr.hs.entrydsm.husky.domain.application.domain.Application;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApplicationBaseRepository<T extends Application> extends CrudRepository<T, Integer> {
-}
--- src/main/java/kr/hs/entrydsm/husky/domain/application/domain/repositories/GeneralApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.hs.entrydsm.husky.domain.application.domain.repositories;
-
-import kr.hs.entrydsm.husky.domain.application.domain.GeneralApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GeneralApplicationRepository extends JpaRepository<GeneralApplication, Integer> {
-}
