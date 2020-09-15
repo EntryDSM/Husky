056a852fb6ca6deb098713df958f780ef525b08e
huewilliams <4879sinwoo@naver.com>
2020-09-15 18:26:47 +0900

    [FIX] generalApplication 수정 사항 반영

--- src/main/java/kr/hs/entrydsm/husky/domain/application/domain/UnGraduatedApplication.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/application/domain/UnGraduatedApplication.java
@@ -3,11 +3,13 @@ package kr.hs.entrydsm.husky.domain.application.domain;
 import kr.hs.entrydsm.husky.domain.application.dto.SetScoreRequest;
 import kr.hs.entrydsm.husky.domain.school.domain.School;
 import kr.hs.entrydsm.husky.domain.user.domain.User;
+import kr.hs.entrydsm.husky.domain.user.dto.SelectTypeRequest;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @Entity(name = "ungraduated_application")
@@ -22,6 +24,12 @@ public class UnGraduatedApplication extends GeneralApplication {
     @PrimaryKeyJoinColumn
     private User user;
 
+    public UnGraduatedApplication update(SelectTypeRequest dto) {
+        if (dto.getGedPassDate() != null)
+            super.updateGraduatedDate(dto.getGraduatedDate().atDay(1));
+        return this;
+    }
+
     public UnGraduatedApplication update(SetScoreRequest dto) {
         updateVolunteerAndAttendance(dto);
         updateGrade(dto);
@@ -33,8 +41,8 @@ public class UnGraduatedApplication extends GeneralApplication {
     }
 
     @Builder(builderMethodName = "unGraduatedApplicationBuilder")
-    public UnGraduatedApplication(Integer receiptCode, String studentNumber, School school, String schoolTel, Integer volunteerTime, Integer fullCutCount, Integer periodCutCount, Integer lateCount, Integer earlyLeaveCount, String korean, String social, String history, String math, String science, String techAndHome, String english) {
-        super(studentNumber, school, schoolTel, volunteerTime, fullCutCount, periodCutCount, lateCount, earlyLeaveCount, korean, social, history, math, science, techAndHome, english);
+    public UnGraduatedApplication(Integer receiptCode, String studentNumber, School school, String schoolTel, LocalDate graduatedDate, Integer volunteerTime, Integer fullCutCount, Integer periodCutCount, Integer lateCount, Integer earlyLeaveCount, String korean, String social, String history, String math, String science, String techAndHome, String english) {
+        super(studentNumber, school, schoolTel, graduatedDate, volunteerTime, fullCutCount, periodCutCount, lateCount, earlyLeaveCount, korean, social, history, math, science, techAndHome, english);
         this.receiptCode = receiptCode;
     }
 
