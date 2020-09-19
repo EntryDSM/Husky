ff19c319dba3bfe7a403aff6f7457acc5b40cefc
huewilliams <4879sinwoo@naver.com>
2020-09-19 12:11:57 +0900

    [ADD] isExist method

--- src/main/java/kr/hs/entrydsm/husky/global/util/Validator.java
+++ src/main/java/kr/hs/entrydsm/husky/global/util/Validator.java
@@ -4,4 +4,9 @@ public class Validator {
     public static boolean isBlank(String target) {
         return target == null || target.isBlank();
     }
+
+    public static boolean isExist(String target) {
+        if(target == null) return false;
+        return !target.isBlank();
+    }
 }
