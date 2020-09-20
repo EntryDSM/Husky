e4b01cb0b906f739cd7a6918632b6f36bc5e420a
huewilliams <4879sinwoo@naver.com>
2020-09-20 12:33:13 +0900

    [ADD] School Service Implement

---
+++ src/main/java/kr/hs/entrydsm/husky/domain/school/service/SchoolServiceImpl.java
@@ -0,0 +1,21 @@
+package kr.hs.entrydsm.husky.domain.school.service;
+
+import kr.hs.entrydsm.husky.domain.school.domain.School;
+import kr.hs.entrydsm.husky.domain.school.domain.repositories.SchoolRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SchoolServiceImpl implements SchoolService {
+
+    private final SchoolRepository schoolRepository;
+
+    @Override
+    public Page<School> search(String eduOffice, String name, Pageable pageable) {
+        return schoolRepository.findBySchoolFullNameContainsAndSchoolNameContains(eduOffice, name, pageable);
+    }
+
+}
