fd7924c9f7651425e82577b959e05f3313ceba28
huewilliams <4879sinwoo@naver.com>
2020-09-20 12:22:01 +0900

    [FIX] intent

--- src/main/java/kr/hs/entrydsm/husky/domain/image/service/ImageService.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/image/service/ImageService.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 public interface ImageService {
+
     String upload(MultipartFile file) throws IOException;
     String generateObjectUrl(String objectName) throws MalformedURLException;
     byte[] getObject(String fileName) throws IOException;
+    
 }
