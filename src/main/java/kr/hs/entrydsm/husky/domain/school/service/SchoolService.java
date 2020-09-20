630c0983667904a4176047d4badf55031207f170
huewilliams <4879sinwoo@naver.com>
2020-09-20 12:32:42 +0900

    [FIX] School Service Class to Interface

--- src/main/java/kr/hs/entrydsm/husky/domain/school/service/SchoolService.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/school/service/SchoolService.java
@@ -1,20 +1,11 @@
 package kr.hs.entrydsm.husky.domain.school.service;
 
 import kr.hs.entrydsm.husky.domain.school.domain.School;
-import kr.hs.entrydsm.husky.domain.school.domain.repositories.SchoolRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class SchoolService {
+public interface SchoolService {
 
-    private final SchoolRepository schoolRepository;
-
-    public Page<School> search(String eduOffice, String name, Pageable pageable) {
-        return schoolRepository.findBySchoolFullNameContainsAndSchoolNameContains(eduOffice, name, pageable);
-    }
+    Page<School> search(String eduOffice, String name, Pageable pageable);
 
 }
