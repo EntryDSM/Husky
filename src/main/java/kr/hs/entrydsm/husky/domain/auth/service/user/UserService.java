c83d6b67278b5b1aa913ae86f41a510710e8f6b2
LeagueLugas <leaguelugas@gmail.com>
2020-09-24 09:41:24 +0900

    AUTH - [FIX] email param to email body

--- src/main/java/kr/hs/entrydsm/husky/domain/auth/service/user/UserService.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/auth/service/user/UserService.java
@@ -2,12 +2,13 @@ package kr.hs.entrydsm.husky.domain.auth.service.user;
 
 import kr.hs.entrydsm.husky.domain.auth.dto.request.AccountRequest;
 import kr.hs.entrydsm.husky.domain.auth.dto.request.ChangePasswordRequest;
+import kr.hs.entrydsm.husky.domain.auth.dto.request.EmailRequest;
 import kr.hs.entrydsm.husky.domain.auth.dto.request.VerifyCodeRequest;
 
 public interface UserService {
     void signUp(AccountRequest accountRequest);
-    void sendSignUpEmail(String email);
+    void sendSignUpEmail(EmailRequest emailRequest);
     void authEmail(VerifyCodeRequest verifyCodeRequest);
-    void sendPasswordChangeEmail(String email);
+    void sendPasswordChangeEmail(EmailRequest emailRequest);
     void changePassword(ChangePasswordRequest changePasswordRequest);
 }
