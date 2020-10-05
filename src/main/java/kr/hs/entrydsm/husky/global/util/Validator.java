3cb82bc89eecab8d3ffa530866e22dd08ef99037
huewilliams <4879sinwoo@naver.com>
2020-10-05 10:01:55 +0900

    [ADD] three validation methods

--- src/main/java/kr/hs/entrydsm/husky/global/util/Validator.java
+++ src/main/java/kr/hs/entrydsm/husky/global/util/Validator.java
@@ -1,13 +1,22 @@
 package kr.hs.entrydsm.husky.global.util;
 
+import java.math.BigDecimal;
+
 public class Validator {
-    public static boolean isBlank(String target) {
-        return target == null || target.isBlank();
+    public static boolean isExists(String target) {
+        return (target != null) && (!target.isBlank());
     }
 
-    public static boolean isExists(String target) {
-        if (target == null) return false;
-        return !target.isBlank();
+    public static boolean isGreaterThanOrEqualTo(Integer target, Integer standard) {
+        return (target != null) && (target >= standard);
+    }
+
+    public static boolean isGreaterThanOrEqualTo(BigDecimal target, BigDecimal standard) {
+        return (target != null) && (target.compareTo(standard) >= 0);
+    }
+
+    public static boolean isNotZero(BigDecimal target) {
+        return (target != null) && (!target.equals(BigDecimal.ZERO));
     }
 
 }
