9c1be3fc15ac05c98aabe0d4705e881a7201083b
huewilliams <4879sinwoo@naver.com>
2020-10-05 10:02:14 +0900

    [ADD] AllCheck method

--- src/main/java/kr/hs/entrydsm/husky/domain/process/service/ProcessService.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/process/service/ProcessService.java
@@ -1,9 +1,11 @@
 package kr.hs.entrydsm.husky.domain.process.service;
 
 import kr.hs.entrydsm.husky.domain.process.dto.ProcessResponse;
+import kr.hs.entrydsm.husky.domain.user.domain.User;
 
 public interface ProcessService {
 
     ProcessResponse getProcess();
+    boolean AllCheck(User user);
 
 }
