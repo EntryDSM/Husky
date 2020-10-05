fd2f128ef1763a61670a02f8efb8c3841e96c9e2
huewilliams <4879sinwoo@naver.com>
2020-10-05 14:03:53 +0900

    [FIX] 메소드명 변경, 파라미터 추가

--- src/main/java/kr/hs/entrydsm/husky/domain/process/service/ProcessService.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/process/service/ProcessService.java
@@ -1,11 +1,12 @@
 package kr.hs.entrydsm.husky.domain.process.service;
 
+import kr.hs.entrydsm.husky.domain.application.domain.CalculatedScore;
 import kr.hs.entrydsm.husky.domain.process.dto.ProcessResponse;
 import kr.hs.entrydsm.husky.domain.user.domain.User;
 
 public interface ProcessService {
 
     ProcessResponse getProcess();
-    boolean AllCheck(User user);
+    boolean allCheck(User user, CalculatedScore score);
 
 }
