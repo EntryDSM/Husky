3e8da57ea26c11e7191fcb52014298c4ac86b9f5
LumpKim <me@jaehoon.kim>
2020-10-12 18:43:50 +0900

    [REFACTOR] PDFExportService::generatePDFApplication

--- src/main/java/kr/hs/entrydsm/husky/domain/pdf/service/PDFExportServiceImpl.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/pdf/service/PDFExportServiceImpl.java
@@ -3,11 +3,12 @@ package kr.hs.entrydsm.husky.domain.pdf.service;
 import kr.hs.entrydsm.husky.domain.application.domain.CalculatedScore;
 import kr.hs.entrydsm.husky.domain.grade.exception.UserNotFoundException;
 import kr.hs.entrydsm.husky.domain.grade.service.GradeCalcService;
-import kr.hs.entrydsm.husky.domain.image.service.ImageService;
 import kr.hs.entrydsm.husky.domain.pdf.converter.ApplicationInfoConverter;
+import kr.hs.entrydsm.husky.domain.pdf.converter.HtmlConverter;
 import kr.hs.entrydsm.husky.domain.pdf.exception.FinalSubmitRequiredException;
 import kr.hs.entrydsm.husky.domain.pdf.exception.UnprocessableEntityException;
-import kr.hs.entrydsm.husky.domain.pdf.util.FontLoader;
+import kr.hs.entrydsm.husky.domain.pdf.processor.TemplateProcessor;
+import kr.hs.entrydsm.husky.domain.pdf.util.PdfMergeUtil;
 import kr.hs.entrydsm.husky.domain.user.domain.Status;
 import kr.hs.entrydsm.husky.domain.user.domain.User;
 import kr.hs.entrydsm.husky.domain.user.domain.repositories.StatusRepository;
@@ -15,25 +16,10 @@ import kr.hs.entrydsm.husky.domain.user.domain.repositories.UserRepository;
 import kr.hs.entrydsm.husky.global.config.security.AuthenticationFacade;
 import kr.hs.entrydsm.husky.global.slack.SlackSenderManager;
 import lombok.RequiredArgsConstructor;
-import org.docx4j.Docx4J;
-import org.docx4j.XmlUtils;
-import org.docx4j.convert.out.FOSettings;
-import org.docx4j.dml.wordprocessingDrawing.Inline;
-import org.docx4j.fonts.Mapper;
-import org.docx4j.jaxb.Context;
-import org.docx4j.model.datastorage.migration.VariablePrepare;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.WordprocessingML.BinaryPartAbstractImage;
-import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
-import org.docx4j.toc.TocHelper;
-import org.docx4j.wml.*;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.xml.bind.JAXBException;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -41,20 +27,20 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class PDFExportServiceImpl implements PDFExportService {
 
-    private Mapper fontMapper;
-
     private final AuthenticationFacade authFacade;
+    private final TemplateProcessor templateProcessor;
+    private final SlackSenderManager slackSenderManager;
+    private final ApplicationInfoConverter applicationInfoConverter;
+
     private final UserRepository userRepository;
     private final StatusRepository statusRepository;
+
     private final GradeCalcService gradeCalcService;
-    private final ImageService imageService;
-    private final ApplicationInfoConverter applicationInfoConverter;
-    private final SlackSenderManager slackSenderManager;
 
     @Override
     public byte[] getPDFApplicationPreview() {
         return userRepository.findById(authFacade.getReceiptCode())
-                .map(user -> generatePDFApplication(user, true))
+                .map(this::generatePDFApplication)
                 .orElseThrow(UserNotFoundException::new);
     }
 
@@ -64,99 +50,41 @@ public class PDFExportServiceImpl implements PDFExportService {
                 .map(user -> {
                     statusRepository.findById(user.getReceiptCode())
                             .filter(Status::isFinalSubmitRequired)
-                            .ifPresent(status -> { throw new FinalSubmitRequiredException(); });
-                    return generatePDFApplication(user, false);
+                            .ifPresent(status -> {
+                                throw new FinalSubmitRequiredException();
+                            });
+                    return generatePDFApplication(user);
                 })
                 .orElseThrow(UserNotFoundException::new);
     }
 
-    private byte[] generatePDFApplication(User user, boolean isPreview) {
-        String templateFileName = setTemplateFileName(user, isPreview);
-
+    private byte[] generatePDFApplication(User user) {
         try {
-            InputStream template = new ClassPathResource(templateFileName).getInputStream();
-            WordprocessingMLPackage mlPackage = WordprocessingMLPackage.load(template);
-            MainDocumentPart documentPart = mlPackage.getMainDocumentPart();
-            VariablePrepare.prepare(mlPackage);
-
-            insertUserProfileImage(user, mlPackage);
             CalculatedScore calculatedScore = gradeCalcService.calcStudentGrade(user);
-            Map<String, String> templateVariables = applicationInfoConverter.applicationToInfo(user, calculatedScore);
-            documentPart.variableReplace(templateVariables);
-            insertNewline(mlPackage);
-            mlPackage.setFontMapper(fontMapper);
+            Map<String, Object> data = applicationInfoConverter.applicationToInfo(user, calculatedScore);
 
-            FOSettings settings = new FOSettings();
-            settings.setWmlPackage(mlPackage);
+            List<String> templates = new LinkedList<>(List.of(
+                    "/application_for_admission",
+                    "/introduction",
+                    "/nonsmoking",
+                    "/admission_agreement"));
 
-            ByteArrayOutputStream result = new ByteArrayOutputStream();
-            Docx4J.toFO(settings, result, Docx4J.FLAG_EXPORT_PREFER_NONXSL);
+            if (!user.isGED() && !user.isCommonApplyType())
+                templates.add(2, "/recommendation");
+
+            ByteArrayOutputStream result = templates.parallelStream()
+                    .map(template -> templateProcessor.process(template, data))
+                    .map(HtmlConverter::convertHtmlToPdf)
+                    .reduce(PdfMergeUtil::concatPdf)
+                    .orElseGet(() -> (ByteArrayOutputStream) ByteArrayOutputStream.nullOutputStream());
+
             return result.toByteArray();
 
         } catch (Exception e) {
             slackSenderManager.send(e);
+            e.printStackTrace();
             throw new UnprocessableEntityException();
         }
-    }
-
-    private String setTemplateFileName(User user, boolean isPreview) {
-        StringBuilder fileName = new StringBuilder("templates/");
-
-        if (isPreview)
-            fileName.append("preview/");
-
-        if (user.isGED())
-            fileName.append("ged_");
-
-        else if (user.isCommonApplyType())
-            fileName.append("common_");
-
-        return fileName.append("application.docx").toString();
-    }
-
-    private void insertUserProfileImage(User user, WordprocessingMLPackage mlPackage) throws Exception {
-        String objectKey = (user.isPhotoEmpty()) ? "default.png" : user.getUserPhoto();
-        byte[] image = imageService.getObject(objectKey);
-        MainDocumentPart documentPart = mlPackage.getMainDocumentPart();
-
-        Body body = documentPart.getJaxbElement().getBody();
-        List<Object> runs = TocHelper.getAllElementsFromObject(body, R.class);
-        for (Object o : runs) {
-            R run = (R) o;
-            String str = XmlUtils.marshaltoString(run);
-            if (str.contains("${userPhoto}")) {
-                P p = (P) run.getParent();
-                p.getContent().remove(run);
-                p.getContent().add(this.createImageRun(mlPackage, image));
-                break;
-            }
-        }
-    }
-
-    private R createImageRun(WordprocessingMLPackage mlPackage, byte[] image) throws Exception {
-        BinaryPartAbstractImage imagePart = BinaryPartAbstractImage.createImagePart(mlPackage, image);
-        Inline inline = imagePart.createImageInline(null, "userPhoto", 0, 1, 979200, 1303200, false);
-        ObjectFactory objectFactory = Context.getWmlObjectFactory();
-        R run = objectFactory.createR();
-        Drawing drawing = objectFactory.createDrawing();
-        run.getContent().add(drawing);
-        drawing.getAnchorOrInline().add(inline);
-        return run;
-    }
-
-    private void insertNewline(WordprocessingMLPackage mlPackage) throws JAXBException {
-        MainDocumentPart part = mlPackage.getMainDocumentPart();
-        String xml = XmlUtils.marshaltoString(part.getJaxbElement());
-        xml = xml.replaceAll("¶", "</w:t><w:br/><w:t>");
-        Object obj = XmlUtils.unmarshalString(xml);
-        part.setJaxbElement((Document) obj);
-    }
-
-    @PostConstruct
-    public void initFonts() {
-        FontLoader fontLoader = new FontLoader();
-        fontLoader.addPhysicalFonts();
-        this.fontMapper = fontLoader.createFontMapperInstance();
     }
 
 }
