de824bd49a7af0e145bab179ce12115ae801c1ed
huewilliams <4879sinwoo@naver.com>
2020-10-13 13:16:39 +0900

    [FIX] Exception naming

--- src/main/java/kr/hs/entrydsm/husky/domain/user/exception/ProcessNotCompletedException.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/user/exception/ProcessNotCompletedException.java
@@ -3,8 +3,8 @@ package kr.hs.entrydsm.husky.domain.user.exception;
 import kr.hs.entrydsm.husky.global.error.exception.BusinessException;
 import kr.hs.entrydsm.husky.global.error.exception.ErrorCode;
 
-public class NotCompletedProcessException extends BusinessException {
-    public NotCompletedProcessException() {
+public class ProcessNotCompletedException extends BusinessException {
+    public ProcessNotCompletedException() {
         super(ErrorCode.NOT_COMPLETED_PROCESS);
     }
 }
--- src/main/java/kr/hs/entrydsm/husky/domain/user/exception/ScheduleNotStartedException.java
+++ src/main/java/kr/hs/entrydsm/husky/domain/user/exception/ScheduleNotStartedException.java
@@ -3,6 +3,6 @@ package kr.hs.entrydsm.husky.domain.user.exception;
 import kr.hs.entrydsm.husky.global.error.exception.BusinessException;
 import kr.hs.entrydsm.husky.global.error.exception.ErrorCode;
 
-public class NotStartedScheduleException extends BusinessException {
-    public NotStartedScheduleException() { super(ErrorCode.NOT_STARTED_SCHEDULE); }
+public class ScheduleNotStartedException extends BusinessException {
+    public ScheduleNotStartedException() { super(ErrorCode.NOT_STARTED_SCHEDULE); }
 }
