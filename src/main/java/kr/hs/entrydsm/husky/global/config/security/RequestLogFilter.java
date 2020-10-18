49d263955031ca91315108a6065cf4cae32ac6f4
LeagueLugas <leaguelugas@gmail.com>
2020-10-18 21:34:07 +0900

    [FIX] - logger

--- src/main/java/kr/hs/entrydsm/husky/global/config/security/RequestLogFilter.java
+++ src/main/java/kr/hs/entrydsm/husky/global/config/security/RequestLogFilter.java
@@ -1,17 +1,13 @@
 package kr.hs.entrydsm.husky.global.config.security;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -29,13 +25,15 @@ public class RequestLogFilter extends OncePerRequestFilter {
 
         Map<String, String> headers = Collections.list(request.getHeaderNames()).stream()
                 .collect(Collectors.toMap(header -> header, request::getHeader));
+        String requestIp = Optional.ofNullable(request.getHeader("x-real-ip"))
+                .orElse("127.0.0.1");
         String headerString = Arrays.toString(headers.entrySet().toArray());
         String paramString = Arrays.toString(getParams(request).entrySet().toArray());
 
         return List.of(
                 request.getMethod(),
                 request.getRequestURI(),
-                request.getHeader("x-real-ip"),
+                requestIp,
                 response.getStatus(),
                 headerString,
                 getBody(request),
