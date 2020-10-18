2fd4b633d1ed26d3c06703638f83e9c6252b14df
LeagueLugas <leaguelugas@gmail.com>
2020-10-19 00:12:20 +0900

    [FIX] - exception catch block

--- src/main/java/kr/hs/entrydsm/husky/global/config/security/WrappedRequest.java
+++ src/main/java/kr/hs/entrydsm/husky/global/config/security/WrappedRequest.java
@@ -33,8 +33,8 @@ public class WrappedRequest extends HttpServletRequestWrapper {
         try {
             InputStream is = request.getInputStream();
             this.rawData = IOUtils.toByteArray(is);
-        } catch (Exception ignored) {
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
