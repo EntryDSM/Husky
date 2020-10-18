59c1e2e0ea1b0ed0fa7d7ed406661336895713ce
LeagueLugas <leaguelugas@gmail.com>
2020-10-18 23:55:45 +0900

    [FIX] - wrapper request

--- src/main/java/kr/hs/entrydsm/husky/global/config/security/WrappedRequest.java
+++ src/main/java/kr/hs/entrydsm/husky/global/config/security/WrappedRequest.java
@@ -1,30 +1,78 @@
 package kr.hs.entrydsm.husky.global.config.security;
 
+import io.micrometer.core.instrument.util.StringUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.entity.ContentType;
+
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class WrappedRequest extends HttpServletRequestWrapper {
 
-    private final String body;
+    private final Charset encoding;
+    private byte[] rawData;
+    private final Map<String, String[]> params = new HashMap<>();
 
-    public WrappedRequest(HttpServletRequest request) throws IOException {
+    public WrappedRequest(HttpServletRequest request) {
         super(request);
+        this.params.putAll(request.getParameterMap());
 
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = request.getReader()) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null)
-                stringBuilder.append(line);
+        String charEncoding = request.getCharacterEncoding();
+        this.encoding = StringUtils.isBlank(charEncoding) ? StandardCharsets.UTF_8 : Charset.forName(charEncoding);
+
+        try {
+            InputStream is = request.getInputStream();
+            this.rawData = IOUtils.toByteArray(is);
+        } catch (Exception ignored) {
+
         }
-        body = stringBuilder.toString();
+    }
+
+    @Override
+    public String getParameter(String name) {
+        String[] paramArray = getParameterValues(name);
+        if (paramArray != null && paramArray.length > 0) {
+            return paramArray[0];
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        return Collections.unmodifiableMap(params);
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return Collections.enumeration(params.keySet());
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] result = null;
+        String[] dummyParamValue = params.get(name);
+
+        if (dummyParamValue != null) {
+            result = new String[dummyParamValue.length];
+            System.arraycopy(dummyParamValue, 0, result, 0, dummyParamValue.length);
+        }
+        return result;
     }
 
     @Override
     public ServletInputStream getInputStream() {
-        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(rawData);
         return new ServletInputStream() {
             @Override
             public boolean isFinished() {
@@ -49,7 +97,11 @@ public class WrappedRequest extends HttpServletRequestWrapper {
 
     @Override
     public BufferedReader getReader() {
-        return new BufferedReader(new InputStreamReader(this.getInputStream()));
+        return new BufferedReader(new InputStreamReader(this.getInputStream(), this.encoding));
+    }
+
+    public String getBody() {
+        return new String(rawData).replaceAll("\r\n", "");
     }
 
 }
